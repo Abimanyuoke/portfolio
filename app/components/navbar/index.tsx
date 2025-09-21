1e5f2d9e19a33d7ad157257dbd691f8df8dc6d2d
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-09-21 13:42:12 +0700

    `Added dark mode support to Hero component, updated navbar and circle transition styles, and modified Prisma logo SVG`

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -67,9 +67,9 @@ export default function Navbar() {
     };
 
     return (
-        <header className="max-w-7xl mx-auto flex justify-between items-center py-4 px-4 lg:px-20" style={{ position: "relative", overflow: "visible" }}>
+        <header className="max-w-7xl mx-auto flex justify-between dark:text-white items-center py-4 px-4 lg:px-20" style={{ position: "relative", overflow: "visible" }}>
             <h1 className="text-3xl md:text-2xl lg:text-3xl font-light m-0">ABICODE</h1>
-            <nav className="hidden md:flex items-center gap-12 font-light">
+            <nav className="hidden md:flex items-center gap-12 font-light text-black dark:text-white">
                 <a href="#" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50 uppercase">Home</a>
                 <a href="#" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50 uppercase">Blog</a>
                 <a href="#" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50 uppercase">Works</a>
