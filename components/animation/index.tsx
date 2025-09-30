e4ad976c2d6b41035ac7807caf7d938df4db55c0
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-09-30 18:59:33 +0700

    `Update component animation index.tsx: changed class name from 'px-4' to 'p-4' in a div element`

--- components/animation/index.tsx
+++ components/animation/index.tsx
@@ -77,7 +77,7 @@ export default function TechStack() {
     return (
         <div>
             <div className="pb-20">
-                <div className="max-w-7xl mx-auto px-4">
+                <div className="max-w-7xl mx-auto p-4">
                     <h3
                         data-aos="fade-right"
                         data-aos-duration="500"
